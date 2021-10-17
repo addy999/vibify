be2b3d6704b5ec22d5b825142a7a6e1b498157d3
addy999 <addy@LAPTOP-C353RCFM.localdomain>
2021-10-17 19:37:18 +0000

    switch to cloud run

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import { writable } from "svelte/store";
 
-export const BASE_URL =  window.location.href.includes('127.0.0.1') ? 'http://127.0.0.1:8000/' : "https://api.vibify.me/";  // api
+export const BASE_URL =  window.location.href.includes('127.0.0.1') ? 'http://127.0.0.1:8000/' : "https://api-ymz3zzn52a-uc.a.run.app/";  // api
 
 interface Store {
     token?: string;
